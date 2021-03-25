a0bfb36cabaeba529f9f942b34c64be6eeb84fa0
khcho902 <khcho902@gmail.com>
2021-03-26 01:14:36 +0900

    complete cpp01/ex01

---
+++ cpp_module01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kycho <kycho@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/26 01:10:35 by kycho             #+#    #+#             */
+/*   Updated: 2021/03/26 01:13:47 by kycho            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
