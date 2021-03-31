c9f2214fd0ca9b0877e544022b6eaf340560a311
khcho902 <khcho902@gmail.com>
2021-03-31 16:49:02 +0900

    complete cpp03/ex01

--- cpp_module03/ex01/main.cpp
+++ cpp_module03/ex01/main.cpp
@@ -6,37 +6,49 @@
 /*   By: kycho <kycho@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 15:28:34 by kycho             #+#    #+#             */
-/*   Updated: 2021/03/31 15:37:58 by kycho            ###   ########.fr       */
+/*   Updated: 2021/03/31 16:45:38 by kycho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main(void)
 {
 
 	FragTrap ft("ft");
+	ScavTrap sc("sc");
 
 	ft.rangedAttack("robot1");
 	ft.meleeAttack("robot2");
 
+	sc.rangedAttack("robot3");
+	sc.meleeAttack("robot4");
+
 	std::cout << std::endl;
 
 	ft.vaulthunter_dot_exe("robot3");
-	ft.vaulthunter_dot_exe("robot3");
-	ft.vaulthunter_dot_exe("robot3");
-	ft.vaulthunter_dot_exe("robot3");
-	ft.vaulthunter_dot_exe("robot3");
+	
+	sc.challengeNewcomer("roboot4");
+	sc.challengeNewcomer("roboot4");
+	sc.challengeNewcomer("roboot4");
+	sc.challengeNewcomer("roboot4");
 
 	std::cout << std::endl;
 
 	ft.takeDamage(55);
 	ft.takeDamage(65);
 
+	sc.takeDamage(55);
+	sc.takeDamage(65);
+
 	std::cout << std::endl;
 
 	ft.beRepaired(30);
 	ft.beRepaired(90);
+	
+	sc.beRepaired(30);
+	sc.beRepaired(90);
 
 	std::cout << std::endl;
 
